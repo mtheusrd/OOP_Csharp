6f40d079abb50eab5b2d8c6f18c153d18e5b2d8b
Matheus Gonçalves <67073149+mtheusrd@users.noreply.github.com>
2023-11-03 16:13:32 +0000

    Add files via upload

---
+++ Projet_Nove/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Nove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseDeDados baseDeDados = new BaseDeDados("BaseDeDados.xml");
+            InterfaceGrafica meuPrograma = new InterfaceGrafica(baseDeDados);
+            meuPrograma.IniciaInterface();
+        }
+    }
+}
